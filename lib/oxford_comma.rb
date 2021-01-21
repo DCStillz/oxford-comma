08f74d650daf044082b99e57e14cdd18c0caf38f
DCStillz <dylan@dcokerstills.com>
2021-01-21 18:03:04 -0500

    commit tree 63d5c8cb3314a76bd47944bab585edc6300c140d

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.count == 2
+        array.join(" and ")
+    elsif array.count > 2
+        last = array.pop
+        new1 = array.join(", ")
+        new1 << ", and #{last}"
+    else
+        array.join        
+    end
 end
